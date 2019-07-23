3bfb55818fc671d73d223bcbd878154ffbd2aa85
Smirnov Aleksandr <smalvad@yandex.ru>
2019-07-24 08:54:19 +0900

    User-info blade file

---
+++ resources/views/krpz/userinfo.blade.php
@@ -0,0 +1,25 @@
+@extends('krpz.layouts.krpz')
+
+@section('content')
+
+    <section class="banner-another"></section>
+
+    <div id="about-us">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-12">
+                    <h3>Вы успешно добавили фото!</h3>
+
+                    <p>
+                        Написать текст чтобы люди не путались
+                    </p>
+
+                    <a data-aos="fade-left" data-aos-delay="500" class="btn btn-success"
+                       href="{{ route('krpz-participate') }}" role="button">Принять участие</a>
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+@endsection
